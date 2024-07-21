816fedaa09dfe7271d27261d104c7e43381c5838
Vincentius Roger Kuswara <vincentiusrkuswara@gmail.com>
2024-07-21 18:35:03 +0800

    feat: integrate vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css';
 import { cn } from '@/lib/utils';
 
 const inter = Inter({ subsets: ['latin'] });
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: 'Vincentius Roger Kuswara',
@@ -41,6 +42,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <Analytics />
     </html>
   );
 }
