86bf54221c38ed31938e173badd9ffe951480c2d
Vincentius Roger Kuswara <vincentiusrkuswara@gmail.com>
2024-07-21 15:31:42 +0800

    feat: responsive education and experience

--- app/(dashboard)/education/details.tsx
+++ app/(dashboard)/education/details.tsx
@@ -3,164 +3,95 @@ import Image from 'next/image';
 import React from 'react';
 
 function Details() {
+  const educationData = [
+    {
+      institution: 'Singapore University of Technology and Design (SUTD)',
+      degree: 'Bachelor of Engineering in Computer Science and Design',
+      duration: 'September 2021 - May 2025',
+      gpa: 'CGPA: 4.96/5.00',
+      awards:
+        'ASEAN UG Scholarship, Honours List 2021/2022, Honours List 2022/2023',
+      logo: '/showcase/sutdlogo.png',
+      showcaseImages: [
+        '/showcase/clickbeat.png',
+        '/showcase/singtel.png',
+        '/showcase/kastogro.jpg',
+      ],
+    },
+    {
+      institution: 'The University of British Columbia (UBC)',
+      degree: 'Exchange Program - Vancouver, Canada',
+      duration: 'September 2023 - December 2023',
+      gpa: 'Semester GPA: 3.93/4',
+      awards: 'Canada-Seed ASEAN Scholarship, $10,200 in stipend',
+      courses:
+        'Applied Machine Learning, Machine Learning and Data Mining, Networks, Cybersecurity',
+      logo: '/showcase/ubclogo.png',
+      showcaseImages: [
+        '/showcase/canada-lyn.png',
+        '/showcase/canada-house.png',
+        '/showcase/canada-ubc2.png',
+      ],
+    },
+  ];
+
   return (
     <>
-      {/* <div className="flex flex-col w-full mt-5">
-        <div className="flex flex-row items-start">
-          <div className="rounded-lg bg-neutral-100 p-2 w-fit">
-            <GraduationCap className="text-neutral-600" />
-          </div>
-          <div className="flex flex-col ml-4">
-            <p className="text-neutral-700 font-medium text-lg mb-2">
-              Singapore University of Technology and Design (SUTD)
-            </p>
-            <p className="text-neutral-500 text-sm">
-              Bachelor of Engineering in Computer Science and Design
-            </p>
-            <p className="text-neutral-500 text-sm">
-              September 2021 - May 2025
-            </p>
-            <p className="text-neutral-500 text-sm">CGPA: 4.96/5.00</p>
-            <p className="text-neutral-500 text-sm">
-              Awards: ASEAN UG Scholarship, Honours List 2021/2022, Honours List
-              2022/2023
-            </p>
-          </div>
-        </div>
-
-        <div className="flex flex-row items-start mt-4">
-          <div className="rounded-lg bg-neutral-100 p-2 w-fit">
-            <GraduationCap className="text-neutral-600" />
-          </div>
-          <div className="flex flex-col ml-4">
-            <p className="text-neutral-700 font-medium text-lg mb-2">
-              The University of British Columbia (UBC)
-            </p>
-            <p className="text-neutral-500 text-sm">Exchange Program</p>
-            <p className="text-neutral-500 text-sm">
-              September 2023 - December 2023
-            </p>
-            <p className="text-neutral-500 text-sm">Semester GPA: 3.93/4</p>
-            <p className="text-neutral-500 text-sm">
-              Awards: Canada-Seed ASEAN Scholarship, $10,200 in stipend
-            </p>
-            <p className="text-neutral-500 text-sm">
-              Courses: Applied Machine Learning, Machine Learning and Data
-              Mining, Networks, Cybersecurity
-            </p>
-          </div>
-        </div>
-      </div> */}
-
-      <div className="flex items-start space-x-6 rounded-lg p-6 transition-all mb-10 mt-5">
-        <div className="flex-shrink-0">
-          <div className="rounded-lg bg-neutral-100 p-2 w-fit">
-            <GraduationCap className="text-neutral-600" />
-          </div>
-        </div>
-        <div className="flex-grow">
-          <div className="flex justify-between items-start">
-            <div>
-              <h3 className="text-xl font-semibold text-neutral-800 mb-2">
-                Singapore University of Technology and Design (SUTD)
-              </h3>
-              <p className="text-neutral-700 font-medium mb-1">
-                Bachelor of Engineering in Computer Science and Design
-              </p>
-              <p className="text-neutral-600 mb-1">September 2021 - May 2025</p>
-              <p className="text-neutral-600 mb-1">CGPA: 4.96/5.00</p>
-              <p className="text-neutral-600 mb-1">
-                Awards: ASEAN UG Scholarship, Honours List 2021/2022, Honours
-                List 2022/2023
-              </p>
-            </div>
-            <div className="flex-shrink-0 w-20 h-20 relative ml-4">
-              <Image
-                src="/showcase/sutdlogo.png"
-                alt="SUTD logo"
-                layout="fill"
-                objectFit="contain"
-                className="rounded-lg"
-              />
+      {educationData.map((edu, index) => (
+        <div
+          key={index}
+          className="flex flex-col lg:flex-row items-start lg:space-x-6 rounded-lg p-4 lg:p-6 transition-all mb-8 lg:mb-10 mt-5"
+        >
+          <div className="flex-shrink-0 mb-4 lg:mb-0">
+            <div className="rounded-lg bg-neutral-100 p-2 w-fit">
+              <GraduationCap className="text-neutral-600" />
             </div>
           </div>
-          <div className="flex flex-row overflow-x-auto max-w-full space-x-5 mt-7 pb-2">
-            {[
-              '/showcase/clickbeat.png',
-              '/showcase/singtel.png',
-              '/showcase/kastogro.jpg',
-            ].map((src, index) => (
-              <div key={index} className="flex-shrink-0 w-60 h-40 relative">
+          <div className="flex-grow w-full">
+            <div className="flex flex-col xl:flex-row justify-between items-start xl:items-center mb-4">
+              <div className="mb-4 lg:mb-0">
+                <h3 className="text-lg lg:text-xl font-semibold text-neutral-800 mb-2">
+                  {edu.institution}
+                </h3>
+                <p className="text-neutral-700 font-medium mb-1">
+                  {edu.degree}
+                </p>
+                <p className="text-neutral-600 mb-1 mt-3">{edu.duration}</p>
+                <p className="text-neutral-600 mb-1">{edu.gpa}</p>
+                <p className="text-neutral-600 mb-1">Awards: {edu.awards}</p>
+                {edu.courses && (
+                  <p className="text-neutral-600">Courses: {edu.courses}</p>
+                )}
+              </div>
+              <div className="flex-shrink-0 w-20 h-20 relative">
                 <Image
-                  src={src}
-                  alt={`Showcase image ${index + 1}`}
+                  src={edu.logo}
+                  alt={`${edu.institution} logo`}
                   layout="fill"
-                  objectFit="cover"
+                  objectFit="contain"
                   className="rounded-lg"
                 />
               </div>
-            ))}
-          </div>
-        </div>
-      </div>
-
-      {/* UBC Education Item */}
-      <div className="flex items-start space-x-6 rounded-lg p-6 transition-all">
-        <div className="flex-shrink-0">
-          <div className="rounded-lg bg-neutral-100 p-2 w-fit">
-            <GraduationCap className="text-neutral-600" />
-          </div>
-        </div>
-        <div className="flex-grow">
-          <div className="flex justify-between items-start">
-            <div>
-              <h3 className="text-xl font-semibold text-neutral-800 mb-2">
-                The University of British Columbia (UBC)
-              </h3>
-              <p className="text-neutral-700 font-medium mb-1">
-                Exchange Program - Vancouver, Canada
-              </p>
-              <p className="text-neutral-600 mb-1">
-                September 2023 - December 2023
-              </p>
-              <p className="text-neutral-600 mb-1">Semester GPA: 3.93/4</p>
-              <p className="text-neutral-600 mb-1">
-                Awards: Canada-Seed ASEAN Scholarship, $10,200 in stipend
-              </p>
-              <p className="text-neutral-600">
-                Courses: Applied Machine Learning, Machine Learning and Data
-                Mining, Networks, Cybersecurity
-              </p>
             </div>
-            <div className="flex-shrink-0 w-20 h-20 relative ml-4">
-              <Image
-                src="/showcase/ubclogo.png"
-                alt="UBC logo"
-                layout="fill"
-                objectFit="contain"
-                className="rounded-lg"
-              />
+            <div className="flex flex-col sm:flex-row overflow-x-auto max-w-full sm:space-x-5 space-y-4 sm:space-y-0 mt-4 lg:mt-7 pb-2">
+              {edu.showcaseImages.map((src, imgIndex) => (
+                <div
+                  key={imgIndex}
+                  className="flex-shrink-0 w-full sm:w-60 h-40 relative"
+                >
+                  <Image
+                    src={src}
+                    alt={`Showcase image ${imgIndex + 1}`}
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-lg"
+                  />
+                </div>
+              ))}
             </div>
           </div>
-          <div className="flex flex-row overflow-x-auto max-w-full space-x-5 mt-7 pb-2">
-            {[
-              '/showcase/canada-lyn.png',
-              '/showcase/canada-house.png',
-              '/showcase/canada-ubc2.png',
-            ].map((src, index) => (
-              <div key={index} className="flex-shrink-0 w-60 h-40 relative">
-                <Image
-                  src={src}
-                  alt={`Showcase image ${index + 1}`}
-                  layout="fill"
-                  objectFit="cover"
-                  className="rounded-lg"
-                />
-              </div>
-            ))}
-          </div>
         </div>
-      </div>
+      ))}
     </>
   );
 }
--- app/(dashboard)/experience/details.tsx
+++ app/(dashboard)/experience/details.tsx
@@ -2,78 +2,111 @@ import Image from 'next/image';
 import React from 'react';
 
 function Details() {
+  const internships = [
+    {
+      company: 'Singapore Telecommunications',
+      logo: '/singtel.png',
+      position: 'Full-stack Developer Intern (Management Associate Program)',
+      duration: 'May 2024 - August 2024',
+      location: 'Singapore',
+      responsibilities: [
+        'Pioneered "common-baseline", a reusable module for GenAI apps, distributed in npm and pip package.',
+        'Architected a versatile GenAI package suite (pip & npm) to standardize 10+ APIs and enforce type-safety to 30+ functions, simplifying installation, updates, and data schema consistency across 4 AI applications.',
+        'Engineered a scalable full-stack architecture using Next.js and Python, allowing migration from a centralized WebSocket approach (h2o.ai wave framework) to support 20x more concurrent users and reduce server crashes by 80%.',
+        'Developed a thread-safe, queue-based message passing system in FastAPI to support non-async library, enabling concurrent WebSocket communication and increasing performance by 10x.',
+        'Created an automated testing system using Selenium, saving developers 100+ hours per month by automating the testing and reporting process to collect KPI data (delay, LLM latency).',
+      ],
+      techStack:
+        'Multi-threading, Python Async, Python Event Loop, WebSocket, FastAPI, Typescript, Next.js/React.js, TailwindCSS, Azure OpenShift Container, Selenium, Bamboo CI/CD.',
+    },
+    {
+      company: 'Terrascope',
+      logo: '/terrascope.png',
+      position: 'Full-stack Developer Intern',
+      duration: 'Jan 2024 – April 2024',
+      location: 'Singapore',
+      responsibilities: [
+        'Collaborated in building Terrascope PCF, an AI-powered platform for companies to calculate, analyze, and optimize product carbon footprints across their product range.',
+        'Developed "nested dropdown with autocomplete" reusable component, contributing to Terrascope Design Library System (DLS) to standardize front-end components across 5+ products using Storybook.js.',
+        'Integrated localization to support multiple languages for the PCF dashboard, allowing users to switch languages with a single click.',
+        'Created a data pipeline integrating Linear and GitHub PR data for company development metrics (PR lifecycle time, feature ticket size, developer productivity).',
+        'Maintained data freshness and efficiency by scheduling the CRON function to run daily using AWS Lambda and AWS CloudWatch, with the database stored in AWS Redshift.',
+        'Designed & built a UI dashboard for data visualization, enabling C-suite executives to conduct SPACE analysis 50% faster, significantly improving observability of technical challenges.',
+        'Overall, reduced manual data processing time by 90% and saved 90% of the cost compared to using a third-party service.',
+      ],
+      techStack:
+        'Storybook, Express.js, Dependency Injection w/ InversifyJS, SOLID Methodology, React.js, Redux State Management, Material UI, CRON function, AWS Lambda, AWS CloudWatch, AWS RedShift.',
+    },
+    {
+      company: 'Datature',
+      logo: '/datature.png',
+      position: 'Front-end Developer Intern',
+      duration: 'May 2023 – August 2023',
+      location: 'Singapore',
+      responsibilities: [
+        'Built a platform enabling users to assign images to single labels using mouse clicks and hotkeys, featuring a visually appealing vignette effect for label display.',
+        'Created a multi-step form interface for configuring external bucket (S3, GCP, Azure) connections, streamlining asset synchronization with the Datature platform.',
+        'Enhanced developer experience (DX) by refactoring the user behavior tracking script into a standardized & reusable module with custom React hooks support.',
+        'Developed confusion matrix data visualization for observing model performance after training, increasing observability for users.',
+        'Refactored 10+ react class-based components to functional-based components, creating a more modular codebase with full React hooks compatibility.',
+        'Developed an automation script to convert 700+ inline CSS styles to Tailwind CSS, enhancing developer experience and code scalability while reducing migration time by 10x.',
+      ],
+      techStack:
+        'React.js, Next.js Pages Router, BlueprintJS, Nivo, Mixpanel CDP, Google Compute Engine, S3 and GCP Bucket, Github Actions & SSH',
+    },
+  ];
+
   return (
     <div className="mt-10">
-      <div className="bg-neutral-200 h-[1px]" />
-      <div className="flex flex-col md:flex-row p-5 w-full md:space-x-4">
-        <div className="flex w-full md:w-2/5 flex-col mb-5 md:mb-0">
-          {/* Company Name and Logo */}
-          <div className="flex flex-row w-full items-center">
-            <div className="flex-shrink-0 w-20 h-20 relative">
-              <Image
-                src="/singtel.png"
-                alt="Singtel logo"
-                layout="fill"
-                objectFit="contain"
-                className="rounded-lg"
-              />
+      {internships.map((internship, index) => (
+        <div key={index}>
+          <div className="bg-neutral-200 h-[1px]" />
+          <div className="flex flex-col lg:flex-row p-5 w-full lg:space-x-4">
+            <div className="flex w-full lg:w-2/5 flex-col mb-5 lg:mb-0">
+              {/* Company Name and Logo */}
+              <div className="flex flex-row w-full items-center pr-5">
+                <div className="flex-shrink-0 w-16 h-16 relative">
+                  <Image
+                    src={internship.logo}
+                    alt={`${internship.company} logo`}
+                    layout="fill"
+                    objectFit="contain"
+                    className="rounded-lg"
+                  />
+                </div>
+                <p className="font-semibold ml-5 flex-grow">
+                  {internship.company}
+                </p>
+              </div>
+              {/* Work Duration */}
+              <div className="text-neutral-500 text-sm mt-2">
+                {internship.duration}
+              </div>
+              <div className="text-neutral-700 text-sm mt-1">
+                {internship.position}
+              </div>
+              <div className="text-neutral-700 text-sm">
+                {internship.location}
+              </div>
             </div>
-            <p className="font-semibold ml-5 flex-grow">
-              Singapore Telecommunications
-            </p>
-          </div>
-          {/* Work Duration */}
-          <div className="text-neutral-500 text-sm mt-2">
-            May 2024 - August 2024
-          </div>
-          <div className="text-neutral-700 text-sm mt-1">
-            Full-stack Developer Intern (Management Associate Program)
-          </div>
-          <div className="text-neutral-700 text-sm">Singapore</div>
-        </div>
 
-        <div className="flex w-full md:flex-1 flex-col">
-          <h3 className="font-semibold mb-2">
-            Responsibilities & Achievements:
-          </h3>
-          <ul className="list-disc pl-5 space-y-2 text-sm">
-            <li>
-              Pioneered &quot;common-baseline&quot;, a reusable module for GenAI
-              apps, distributed in npm and pip package.
-            </li>
-            <li>
-              Architected a versatile GenAI package suite (pip & npm) to
-              standardize 10+ APIs and enforce type-safety to 30+ functions,
-              simplifying installation, updates, and data schema consistency
-              across 4 AI applications.
-            </li>
-            <li>
-              Engineered a scalable full-stack architecture using Next.js and
-              Python, allowing migration from a centralized WebSocket approach
-              (h2o.ai wave framework) to support 20x more concurrent users and
-              reduce server crashes by 80%.
-            </li>
-            <li>
-              Developed a thread-safe, queue-based message passing system in
-              FastAPI to support non-async library, enabling concurrent
-              WebSocket communication and increasing performance by 10x.
-            </li>
-            <li>
-              Created an automated testing system using Selenium, saving
-              developers 100+ hours per month by automating the testing and
-              reporting process to collect KPI data (delay, LLM latency).
-            </li>
-          </ul>
-          <div className="mt-4">
-            <h3 className="font-semibold mb-2">Tech Stack:</h3>
-            <p className="text-sm">
-              Multi-threading, Python Async, Python Event Loop, WebSocket,
-              FastAPI, Typescript, Next.js/React.js, TailwindCSS, Azure
-              OpenShift Container, Selenium, Bamboo CI/CD
-            </p>
+            <div className="flex w-full lg:flex-1 flex-col">
+              <h3 className="font-semibold mb-2">
+                Responsibilities & Achievements:
+              </h3>
+              <ul className="list-disc pl-5 space-y-2 text-sm">
+                {internship.responsibilities.map((resp, idx) => (
+                  <li key={idx}>{resp}</li>
+                ))}
+              </ul>
+              <div className="mt-4">
+                <h3 className="font-semibold mb-2">Tech Stack:</h3>
+                <p className="text-sm">{internship.techStack}</p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
